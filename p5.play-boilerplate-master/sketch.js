a8da936f281b07c723fcf4a8cd4e81f612f56bb8
Jonathan2325 <85315287+Jonathan2325@users.noreply.github.com>
2021-08-31 15:09:05 -0500

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,10 @@
+// Sorry for submitting blank, it seems like the files i downloaded were blank with no image files in it
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
